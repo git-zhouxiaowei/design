942a1b24ad49f7b5a270f30f0ad904b114c0ab2e
git-zhouxiaowei <283922486@qq.com>
2021-01-10 20:34:50 +0800

    PC首页静态样式完善

--- src/main/resources/static/js/front/side-js.js
+++ src/main/resources/static/js/front/side-js.js
@@ -1,30 +1,31 @@
-$(function(){
-    move();
+$(function () {
+    move("scrollsidebar");
 });
 
-window.onscroll = function(){
-    move();
+window.onscroll = function () {
+    move("scrollsidebar");
 }
 
 var timer = null;
-function startMove(argument) {
-    var scrollsidebar = document.getElementById("scrollsidebar");
+function startMove(argument, e) {
+    var sidebar = document.getElementById(e);
     clearInterval(timer);
-    timer = setInterval(function() {
-        var speed = (argument - scrollsidebar.offsetTop) / 4;
+    timer = setInterval(function () {
+        var speed = (argument - sidebar.offsetTop) / 4;
         speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
         if (argument == scrollsidebar.offsetTop) {
             clearInterval(timer);
         } else {
-            scrollsidebar.style.top = scrollsidebar.offsetTop + speed + "px";
+            sidebar.style.top = sidebar.offsetTop + speed + "px";
         }
     }, 60);
 };
 
-function move() {
-    var scrollsidebar = document.getElementById("scrollsidebar");
+function move(e) {
+    var sidebar = document.getElementById(e);
     var scrolltop = document.body.scrollTop || document.documentElement.scrollTop;
-    startMove(
-        parseInt((document.documentElement.clientHeight - scrollsidebar.offsetHeight) / 2 + scrolltop)
-    );
+    startMove(parseInt((document.documentElement.clientHeight - sidebar.offsetHeight) / 2 + scrolltop), e);
 }
+
+
+
